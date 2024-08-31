2679465e8ab6425cbf687be501c7c43ade9312cd
Ksawery <47505116+ksawery29@users.noreply.github.com>
2024-08-31 17:56:57 +0200

    refactor: cleanup srcdoc

--- src/iframe/srcdoc.ts
+++ src/iframe/srcdoc.ts
@@ -17,52 +17,40 @@ const generateSrcdoc = (
                 toStr.indexOf(")")
             );
 
-            const f = `function ${key}(${args}) {
-            return window.parent.postMessage({type: "function", name: "${key}", args: Array.from(arguments)}, "*");
-        }`;
-
-            // post processing: remove newlines and extra spaces
-            return f.replace(/\n/g, "").replace(/\s+/g, " ");
+            const f = `function ${key}(${args}) {return window.parent.postMessage({type: "function", name: "${key}", args: Array.from(arguments)}, "*");}`;
+            return f;
         });
     }
 
-    return `
-<!DOCTYPE html>
-<html lang="en">
-<body>
-    <script>
-        (() => {
-            // override the default console.[log/error/warn] methods to send messages to the parent window
-            ["log", "error", "warn"].forEach(method => {
-                let original = console[method]; // save a copy
-                console[method] = (...args) => {
-                    try {
-                        // remove non-serializable objects like window or storage
-                        const serializableArgs = args.map(arg => {
-                            try {
-                                JSON.stringify(arg); // just to check if its serializable
-                                return arg;
-                            } catch (e) {
-                                return Object.prototype.toString.call(arg)
-                            }
-                        });
-                        window.parent.postMessage({type: "console", method, args: serializableArgs}, "*");
-                        original.apply(console, args);
-                    } catch (e) {
-                        original.error("isolated-js:", e)
-                    }
+    // override the default console.[log/error/warn] methods to send messages to the parent window
+    const customLogHandler = `
+    (() => {
+        const methods = ["log", "error", "warn"];
+        methods.forEach((method) => {
+            const original = console[method].bind(console);
+            console[method] = (...args) => {
+                try {
+                    const serializableArgs = args.map((arg) => {
+                        try {
+                            JSON.stringify(arg);
+                            return arg;
+                        } catch {
+                            return Object.prototype.toString.call(arg);
+                        }
+                    });
+
+                    window.parent.postMessage({ type: "console", method, args: serializableArgs }, "*");
+                } catch (e) {
+                    original("isolated-js:", e);
                 }
-            });
+            };
+        });
+    })();
+    `;
 
-            // all predefined functions are below, if any
-            ${getters && getters.join("\n")}
-
-            ${userCode}
-        })()
-    </script>
-</body>
-</html>
-`;
+    return `<script>(() => {${customLogHandler};${
+        getters && getters.join(" ")
+    };${userCode};})()</script>`;
 };
 
 export default generateSrcdoc;
