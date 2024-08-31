f45c0aa9e2a9e0dda6659eeb89ab17555660e746
Ksawery <47505116+ksawery29@users.noreply.github.com>
2024-08-31 21:09:53 +0200

    feat: sanitize, add random id to each iframe

--- src/iframe/event-handler.ts
+++ src/iframe/event-handler.ts
@@ -6,72 +6,81 @@ export default function eventHandler(
     iframe: HTMLIFrameElement,
     settings: IsolatedSettings,
     onFinished: () => void
-) {
+): () => void {
+    const handler = (event: MessageEvent<EventHandlerType>) => {
+        // ensure this came from the same origin
+        if (event.source !== iframe.contentWindow) return;
+
+        // get the message and parse it
+        try {
+            switch (event.data.type) {
+                case "console":
+                    if (event.data.method == undefined) {
+                        console.error(
+                            "isolated-js: event.data.method is somehow undefined. this is probably a bug, please report it"
+                        );
+                        return;
+                    }
+
+                    if (settings.onConsole && event.data.args[0])
+                        settings.onConsole(
+                            event.data.method,
+                            event.data.args[0]
+                        );
+                    break;
+                case "function":
+                    if (settings.predefinedFunctions == undefined) {
+                        console.error(
+                            "isolated-js: predefined functions are undefined but got requested"
+                        );
+                        return;
+                    }
+
+                    const functionName = event.data.name;
+                    if (functionName == undefined) {
+                        console.error(
+                            "isolated-js: got empty name for function"
+                        );
+                        return;
+                    }
+                    const functionArgs = event.data.args || [];
+
+                    // check if that function requested is a thing
+                    if (settings.predefinedFunctions[functionName]) {
+                        settings.predefinedFunctions[functionName](
+                            ...functionArgs
+                        );
+                    } else {
+                        console.error("isolated-js: unknown function");
+                    }
+
+                    break;
+                case "finished_execution":
+                    onFinished();
+                    break;
+                default:
+                    console.warn(
+                        "isolated-js: unknown action got in the event handler!",
+                        event.data
+                    );
+            }
+        } catch (error) {
+            console.error(
+                "isolated-js: failed to parse the message from iframe",
+                error
+            );
+        }
+    };
+
     window.addEventListener(
         "message",
         (event: MessageEvent<EventHandlerType>) => {
-            // ensure this came from the same origin
-            if (event.source !== iframe.contentWindow) return;
-
-            // get the message and parse it
-            try {
-                switch (event.data.type) {
-                    case "console":
-                        if (event.data.method == undefined) {
-                            console.error(
-                                "isolated-js: event.data.method is somehow undefined. this is probably a bug, please report it"
-                            );
-                            return;
-                        }
-
-                        if (settings.onConsole && event.data.args[0])
-                            settings.onConsole(
-                                event.data.method,
-                                event.data.args[0]
-                            );
-                        break;
-                    case "function":
-                        if (settings.predefinedFunctions == undefined) {
-                            console.error(
-                                "isolated-js: predefined functions are undefined but got requested"
-                            );
-                            return;
-                        }
-
-                        const functionName = event.data.name;
-                        if (functionName == undefined) {
-                            console.error(
-                                "isolated-js: got empty name for function"
-                            );
-                            return;
-                        }
-                        const functionArgs = event.data.args || [];
-
-                        // check if that function requested is a thing
-                        if (settings.predefinedFunctions[functionName]) {
-                            settings.predefinedFunctions[functionName](
-                                ...functionArgs
-                            );
-                        } else {
-                            console.error("isolated-js: unknown function");
-                        }
-
-                        break;
-                    case "finished_execution":
-                        onFinished();
-                        break;
-                    default:
-                        console.warn(
-                            "isolated-js: unknown action got in the event handler!",
-                            event.data
-                        );
-                }
-            } catch (error) {
-                console.error(
-                    "isolated-js: failed to parse the message from iframe",
-                    error
-                );
-            }
+            handler(event);
         }
     );
+
+    return () => {
+        console.log("remove");
+        window.removeEventListener("message", handler);
+    };
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import eventHandler from "./iframe/event-handler.js";
 import generateSrcdoc from "./iframe/srcdoc.js";
 import { type IsolatedSettings } from "./types/main";
+import DOMPurify from "dompurify";
 
 export default class Isolated {
     settings: IsolatedSettings;
     userCode: string;
+    uniqueId: string;
+
+    private sanitize(userCode: string): string {
+        // really just to be safe, but idk if this is necessary mainly because the iframe is sandboxed + javascript only
+        // but it's better to be safe than sorry :)
+        return DOMPurify.sanitize(userCode, {
+            ALLOWED_TAGS: [],
+            ALLOWED_ATTR: [],
+        });
+    }
 
     constructor(userCode: string, settings?: IsolatedSettings) {
         this.settings = settings ?? {};
@@ -13,7 +24,10 @@ export default class Isolated {
         this.settings.timeout = this.settings.timeout ?? 5000;
         this.settings.removeAfterExecution =
             this.settings.removeAfterExecution ?? true;
-        this.userCode = userCode;
+        this.userCode = this.sanitize(userCode);
+        this.uniqueId = `isolated-js-iframe-${Math.random()
+            .toString(36)
+            .slice(2, 11)}`;
     }
 
     public async start(): Promise<HTMLIFrameElement> {
@@ -23,18 +37,16 @@ export default class Isolated {
         );
 
         let iframe: HTMLIFrameElement;
-        let exists: boolean = false;
 
         // the iframe might exist from previous executions
-        const mightExist = document.getElementById("isolated-js-iframe");
+        const mightExist = document.getElementById(this.uniqueId);
         if (mightExist) {
-            iframe = mightExist as HTMLIFrameElement;
-            exists = true;
-        } else {
-            // create a new iframe
-            iframe = document.createElement("iframe");
-            if (this.settings.beforeInit) this.settings.beforeInit(iframe); // run the beforeInit function
+            mightExist.remove();
         }
+
+        // create a new iframe
+        iframe = document.createElement("iframe");
+        if (this.settings.beforeInit) this.settings.beforeInit(iframe); // run the beforeInit function
 
         return await new Promise<HTMLIFrameElement>((resolve, reject) => {
             let id: number;
@@ -42,10 +54,7 @@ export default class Isolated {
                 id = setTimeout(() => {
                     const err = () => {
                         if (this.settings.onConsole)
-                            this.settings.onConsole(
-                                "error",
-                                "execution timed out"
-                            );
+                            this.settings.onConsole("error", "canceled");
                         console.error("isolated-js: execution timed out");
                         reject("execution timed out");
                     };
@@ -56,18 +65,16 @@ export default class Isolated {
                 }, this.settings.timeout);
             }
 
-            if (!exists) {
-                eventHandler(iframe, this.settings, () => {
-                    if (this.settings.timeout != -1) clearTimeout(id);
-                    if (this.settings.removeAfterExecution) iframe.remove();
+            const cleanup = eventHandler(iframe, this.settings, () => {
+                if (this.settings.timeout != -1) clearTimeout(id);
+                if (this.settings.removeAfterExecution) iframe.remove();
 
-                    resolve(iframe);
-                }); // initialize the event handler
-            }
+                resolve(iframe);
+            }); // initialize the event handler
 
             iframe.setAttribute("sandbox", "allow-scripts");
             iframe.setAttribute("srcDoc", srcDoc);
-            iframe.setAttribute("id", "isolated-js-iframe");
+            iframe.setAttribute("id", this.uniqueId);
             iframe.onerror = (error) => {
                 reject(error);
             };
@@ -77,6 +84,8 @@ export default class Isolated {
 
             // append the iframe to the body
             document.body.appendChild(iframe);
+
+            return cleanup();
         });
     }
 }
